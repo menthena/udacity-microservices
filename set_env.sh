c4a067f1d07e42e72fa89acfd52c4e0cc1b401ec
Ahmet Atasoy <menthena@gmail.com>
2021-01-31 15:31:00 +0300

    Initial commit

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
+export POSTGRES_PASSWORD=a
 export POSTGRES_HOST=udacitydb.cjvf92dxt6gy.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=udacitydb
 export AWS_BUCKET=
